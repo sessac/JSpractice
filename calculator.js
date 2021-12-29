3918631febb1526cf8bb34c2a9c9054dcc598c50
sessac <96456334+sessac@users.noreply.github.com>
2021-12-29 17:41:57 +0900

    calculator

---
+++ calculator.js
@@ -0,0 +1,19 @@
+const calculator = {
+    add: function (a, b) {
+        console.log(a + b);
+},  minus: function (a, b) {
+        console.log(a - b);
+},  div: function (a, b) {
+        console.log(a / b);
+},  multi: function (a, b) {
+    console.log(a * b);
+},  powerOf: function (a, b) {
+    console.log(a ** b);
+}
+}
+
+calculator.add(1, 2);
+calculator.minus(5, 2);
+calculator.div(30, 5);
+calculator.multi(16, 21);
+calculator.powerOf(10, 4);
