225823b4ffa553fa71d3223a5d839df4f517a196
sessac <96456334+sessac@users.noreply.github.com>
2022-01-04 16:56:26 +0900

    css js event

--- event.js
+++ event.js
@@ -1,29 +1,26 @@
-const endOfYear = document.querySelector("div.today h1");
-const newYear = document.querySelector("div.happy h1");
+const goHome = document.querySelector("div.think h1");
 
 
-function handleEndClick() {
-  const currentEndColor = endOfYear.style.color;
-  let ChangeEndColor;
-  if (currentEndColor === "blue") {
-    ChangeEndColor = "yellow";
-  } else {
-    ChangeEndColor = "blue";
-  }
-    endOfYear.style.color = ChangeEndColor;
+// function handleClick() {
+//   const clickedClass = "active nice-font";
+//   if (goHome.className === clickedClass) {
+//     goHome.className = "";
+//   } else {
+//     goHome.className = clickedClass;
+//   }
+// }
+   
+// function handleClick() {
+//   const clickedClass = "active";
+//   if (goHome.classList.contains(clickedClass)) {
+//     goHome.classList.remove(clickedClass);
+//   } else {
+//     goHome.classList.add(clickedClass);
+//   }
+// }
+
+function handleClick() {
+  goHome.classList.toggle("active");
 }
 
-
-function handleNewClick() {
-  const currentNewColor = newYear.style.color;
-  let ChangeNewColor;
-  if (currentNewColor === "red") {
-    ChangeNewColor = "green";
-  } else {
-    ChangeNewColor = "red";
-  }
-    newYear.style.color = ChangeNewColor;
-}
-
-    endOfYear.addEventListener("click", handleEndClick);
-    newYear.addEventListener("click", handleNewClick);
+    goHome.addEventListener("click", handleClick);
